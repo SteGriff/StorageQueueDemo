9716df10bc39997893f6232eaf3602a871f60d14
Stephen Griffiths <github@stegriff.co.uk>
2017-06-03 16:21:23 +0000

    init

---
+++ StorageQueueDemo/QueueProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageQueueDemo
+{
+    public class QueueProvider
+    {
+        private string _connectionString;
+
+        public QueueProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public CloudQueue GetQueue(string queueName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_connectionString);
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference(queueName);
+
+            queue.CreateIfNotExists();
+            return queue;
+        }
+    }
+}
